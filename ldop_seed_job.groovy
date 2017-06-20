68abccc55f0ce8475e22513ff77debf044187552
Justin Bankes <jbankes@mail.csuchico.edu>
2017-06-19 17:36:13 -0700

    Started the seed job.

--- ldop_seed_job.groovy
+++ ldop_seed_job.groovy
@@ -2,3 +2,37 @@
 // authored: Justin Bankes on 6.19.17
 // Last Modified:
 folder('ldop')
+
+def ldopImages = ['ldop-docker-compose']
+
+ldopImages.each {
+  ldopImageName->
+
+  def repoURL = "https://github.com/liatrio/" + ldopImageName + ".git"
+  // def projectURL = "https://github.com/liatrio/" + ldopImageName 
+  def singleImageJobName = ldopImageName + '-1-build'
+
+  job('ldop/' + singleImageJobName){
+    description('This job was created with automation. Manual edits to this job are discouraged.')
+    wrappers {
+      colorizeOutput()
+    }
+    properties {
+      githubProjectUrl(repoURL)
+    }
+    scm {
+      git {
+        remote {
+          url(repoURL)
+        }
+      }
+    }
+    triggers {
+      cron('@weekly')
+      githubPush()
+    }
+    steps {
+      shell('echo Hello World!')
+    }
+  }
+}
