6b632aedf7f2396e553cb84a696010036e56e094
ebracho <eddieb@liatrio.com>
2017-07-11 11:40:03 -0700

    LDOP-86 Added topic build and integration jobs

--- ldop_seed_job.groovy
+++ ldop_seed_job.groovy
@@ -44,8 +44,16 @@ ldopImages.each {
       githubPush()
     }
     steps {
-      shell("docker build -t jbankes/${ldopImageName}:latest .")
-      shell("docker push jbankes/${ldopImageName}:latest")
+      shell(
+"""\
+#!/bin/bash
+
+TOPIC=\"\${GIT_BRANCH#*/}\"
+
+docker build -t jbankes/${ldopImageName}:\${TOPIC} .
+docker push jbankes/${ldopImageName}:\${TOPIC}
+"""
+      )
     }
     publishers {
       downstreamParameterized {
@@ -54,6 +62,7 @@ ldopImages.each {
           parameters {
             predefinedProp('IMAGE_VERSION', '\${GIT_TAG_NAME}')
             predefinedProp('IMAGE_NAME', ldopImageName)
+            predefinedProp('TOPIC', '\${GIT_BRANCH}')
           }
         }
       }
@@ -93,6 +102,7 @@ job('ldop/ldop-integration-testing') {
   parameters {
     textParam('IMAGE_VERSION')
     textParam('IMAGE_NAME')
+    textParam('TOPIC')
   }
   properties {
     githubProjectUrl(repoURL)
@@ -106,8 +116,16 @@ job('ldop/ldop-integration-testing') {
     }
   }
   steps {
-    shell('sed -i "/liatrio\\/${IMAGE_NAME}/c\\     image: liatrio/${IMAGE_NAME}:latest" docker-compose.yml')
-    shell('./test/integration/run-integration-test.sh')
+    shell(
+"""\
+TOPIC="${TOPIC#*/}"
+git checkout ${TOPIC}
+sed -i "/liatrio\/${IMAGE_NAME}/c\    image: jbankes/${IMAGE_NAME}:${TOPIC}" docker-compose.yml
+export TF_VAR_branch_name="${TOPIC}"
+./test/integration/run-integration-test.sh
+"""
+
+    )
   }
   publishers {
     downstreamParameterized {
@@ -127,9 +145,10 @@ job('ldop/ldop-image-deploy') {
   parameters {
     textParam('IMAGE_VERSION')
     textParam('IMAGE_NAME')
+    textParam('TOPIC')
   }
   steps {
-    shell('docker tag jbankes/${IMAGE_NAME}:latest jbankes/${IMAGE_NAME}:${IMAGE_VERSION}')
+    shell('docker tag jbankes/${IMAGE_NAME}:${TOPIC} jbankes/${IMAGE_NAME}:${IMAGE_VERSION}')
     shell('docker push jbankes/${IMAGE_NAME}:${IMAGE_VERSION}')
   }
 }
